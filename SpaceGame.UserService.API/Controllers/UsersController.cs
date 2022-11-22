dadb9ec534486b0fc8f0b36e81e19632f891293d
Sofiia Rutska <rutska.sofia@gmail.com>
2022-11-22 22:19:24 +0200

    Add project files.

---
+++ SpaceGame.UserService.API/Controllers/UsersController.cs
@@ -0,0 +1,93 @@
+﻿using Amazon.AspNetCore.Identity.Cognito;
+using Amazon.CognitoIdentityProvider.Model;
+using Amazon.Extensions.CognitoAuthentication;
+using SpaceGame.DAL.Models;
+using SpaceGame.UserService.API.Models;
+using SpaceGame.UserService.API.Services;
+using SpaceGames.UserService.Api.Services;
+
+namespace SpaceGames.UserService.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UsersController : Controller
+    {
+        IUserService _userService;
+        private readonly CognitoUserManager<CognitoUser> _userManager;
+
+
+        public UsersController(IUserService userService, UserManager<CognitoUser> userManager)
+        {
+            _userService = userService;
+            _userManager = (CognitoUserManager<CognitoUser>)userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _userService.Get(activeOnly: true);
+            return Ok(users);
+        }
+
+        [HttpGet("{email}")]
+        public async Task<IActionResult> GetUser([FromRoute] string email)
+        {
+            var user = await _userService.GetByEmail(email);
+            return Ok(user);
+        }
+
+        [HttpPut("{email}")]
+        public async Task<IActionResult> EditUser([FromRoute]string email, [FromBody] UserProfileRequestModel model)
+        {
+            CognitoIdentityHelper.TryGetEmailFromClaim(base.User, out string emailFromToken);
+            if (email != emailFromToken)
+            {
+                return Forbid();
+            }
+
+            await _userService.UpdateUser(email, model);
+
+            return Ok();
+        }
+
+        [HttpPost("{email}/avatar")]
+        public async Task<IActionResult> UploadAvatar([FromRoute]string email, UserAvatarRequestModel file)
+        {
+            CognitoIdentityHelper.TryGetEmailFromClaim(base.User, out string emailFromToken);
+            if(email != emailFromToken)
+            {
+                return Forbid();
+            }
+
+            await _userService.UpdateUser(email, file);
+
+            return Ok();
+        }
+
+        // Home work
+        [HttpDelete("{email}")]
+        public async Task<IActionResult> DeleteUser([FromRoute]string email)
+        {
+            CognitoIdentityHelper.TryGetEmailFromClaim(base.User, out string emailFromToken);
+            
+            if (email != emailFromToken)
+            {
+                return Forbid();
+            }
+
+            var cognitoUser = await _userManager.FindByNameAsync(emailFromToken);
+            if (cognitoUser == null)
+            {
+               return NotFound("Cognito user is not found");
+            }
+            await _userManager.DeleteAsync(cognitoUser);
+
+            await _userService.DeleteUser(email);
+            // delete from DynamoDB calling _userService
+            // delete from Cognito using CognitoUserManager
+
+            return Ok();
+        }
+    }
+}
